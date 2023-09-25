ec2fc347ea7782211dcdcd5549f8d515a13e429c
Samir <samir_zulfiqar@yahoo.com>
2023-09-25 11:59:25 -0400

    added header and form input for players

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("hello,world")
+const gameBoardObj = (() => {
+    const gameBoardArray = [];
+    for (let i = 0; i<9; i++) {
+        let square = i;
+        gameBoardArray.push(square)
+    }
+
+    return {gameBoardArray};
+})();
+
+console.log(gameBoardObj)
+
+const startBtn = document.querySelector(".start-game");
+const form = document.querySelector('form');
+const body = document.body;
+
+startBtn.addEventListener('click', () => {
+    if (form.checkValidity()) 
+    body.removeChild(form)
+})
